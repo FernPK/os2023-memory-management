aa43fa658297993fc8a6d77bce57598514d83575
FernPK <pornnapat5563@gmail.com>
2023-08-11 09:00:55 +0700

    first commit

---
+++ memoryManagement.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// show that stack is growing downwards and heap is growing upwards
+
+int cnt = 0;
+
+void recursion(int x){
+    if(x == 0){
+        return;
+    }
+    int top;
+    printf("Stack Top: %p\n", &top);
+    recursion(x-1);
+}
+
+void mallocInHeap(int x){
+    if(x == 0){
+        return;
+    }
+    int *hp = (int*)malloc(sizeof(int));
+    printf("Heap Top: %p\n", hp);
+    mallocInHeap(x-1);
+    free(hp);
+}
+
+int *hp2 = NULL;
+void mallocInHeap2(int x){
+    hp2 = (int*)malloc(sizeof(int) * x);
+    for(int i = 0; i < x; i++){
+        printf("Heap2 Top: %p\n", &hp2[i]);
+    }
+    free(hp2);
+}
+
+void stackOverflow(){
+    int newVar;
+    int *newMalloc = (int*)malloc(sizeof(int));
+    printf("Stack Growing: %p\n", &newVar);
+    printf("Heap Growing: %p\n", newMalloc);
+    stackOverflow();
+}
+
+char name = 'a';
+
+int main(){
+    printf("1st global var (cnt: int): %p\n", &cnt);
+    printf("2nd global var (hp2: pointer): %p\n", &hp2);
+    printf("3rd global var (name: char): %p\n", &name);
+    recursion(3);
+    mallocInHeap(3);
+    mallocInHeap2(3);
+    stackOverflow();
+    return 0;
+}
