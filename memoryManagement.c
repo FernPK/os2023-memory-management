720165791996128a3b553ddc15342a7a756ff528
FernPK <pornnapat5563@gmail.com>
2023-08-16 08:21:55 +0700

    remove a malloc function

--- memoryManagement.c
+++ memoryManagement.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// show that stack is growing downwards and heap is growing upwards
-
 int cnt = 0;
 
 void recursion(int x){
@@ -14,23 +12,16 @@ void recursion(int x){
     recursion(x-1);
 }
 
-int *hp = NULL;
-void mallocInHeap(int x){
-    hp = (int*)malloc(sizeof(int) * x);
-    for(int i = 0; i < x; i++){
-        printf("Heap Top: %p\n", &hp[i]);
-    }
-    free(hp);
-}
+int *ptr;
 
-void mallocInHeap2(int x){
+void mallocInHeap(int x){
     if(x == 0){
         return;
     }
-    int *hp2 = (int*)malloc(sizeof(int));
-    printf("Heap2 Top: %p\n", hp2);
-    mallocInHeap2(x-1);
-    free(hp2);
+    int *hp = (int*)malloc(sizeof(int));
+    printf("Heap Top: %p\n", hp);
+    mallocInHeap(x-1);
+    free(hp);
 }
 
 void stackOverflow(){
@@ -45,11 +36,10 @@ char name = 'a';
 
 int main(){
     printf("1st global var (cnt: int): %p\n", &cnt);
-    printf("2nd global var (hp: pointer): %p\n", &hp);
+    printf("2nd global var (ptr: pointer): %p\n", &ptr);
     printf("3rd global var (name: char): %p\n", &name);
     recursion(3);
     mallocInHeap(3);
-    mallocInHeap2(3);
     stackOverflow();
     return 0;
 }
